55ef99f490d063c40f34582043c7b2f04b11bcf6
Thomas Schwan <tschwan@gmail.com>
2026-03-07 12:35:51 +0100

    fix(footer): solve CORS/file-protocol issue by injecting footer content via nav.js

--- site/assets/js/nav.js
+++ site/assets/js/nav.js
@@ -37,17 +37,62 @@
                 link.classList.add("is-active");
             }
         });
-    // --- Load Footer dynamically ---
-    const footer = document.getElementById("main-footer");
-    if (footer) {
-        fetch("footer.html")
-            .then((response) => {
-                if (!response.ok) throw new Error("Could not load footer.");
-                return response.text();
-            })
-            .then((data) => {
-                footer.innerHTML = data;
-            })
-            .catch((err) => console.error(err));
+    // --- Inject Footer ---
+    const footerContainer = document.getElementById("main-footer");
+    if (footerContainer) {
+        footerContainer.innerHTML = `
+<div class="container">
+    <div class="site-footer__grid">
+        <div>
+            <div class="site-footer__brand">
+                <span class="material-symbols-outlined site-footer__brand-icon">terrain</span>
+                <span class="site-footer__brand-name">Malerweg</span>
+            </div>
+            <p class="site-footer__desc">
+                Unser persönlicher Begleiter für die
+                Malerweg-Wanderung 2026 in der Sächsischen Schweiz.
+            </p>
+        </div>
+        <div class="site-footer__col">
+            <h5>Informationen</h5>
+            <ul>
+                <li><a href="anreise.html">Anreise &amp; Parken</a></li>
+                <li><a href="organisation.html">Tagesablauf</a></li>
+                <li><a href="navigation.html">Empfohlene Apps</a></li>
+                <li><a href="https://www.saechsische-schweiz.de/malerweg/" target="_blank" rel="noopener">Offizielle Webseite</a></li>
+                <li><a href="https://www.saechsische-schweiz.de/gut-zu-wissen/aktuelles" target="_blank" rel="noopener">Nationalpark-Infos</a></li>
+            </ul>
+        </div>
+        <div class="site-footer__col">
+            <h5>Downloads</h5>
+            <ul>
+                <li><a href="downloads.html">GPX Tracks</a></li>
+                <li><a href="downloads.html">Zeitplan</a></li>
+            </ul>
+        </div>
+        <div class="site-footer__col site-footer__emergency">
+            <h5>Notfall</h5>
+            <ul>
+                <li class="site-footer__emergency-item">
+                    <span class="material-symbols-outlined">call</span>
+                    Rettungsdienst: 112
+                </li>
+                <li class="site-footer__emergency-item">
+                    <span class="material-symbols-outlined">local_hospital</span>
+                    Polizei: 110
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="site-footer__bottom">
+        <p class="site-footer__copy">
+            © 2026 Stagely - Your hike perfectly staged
+        </p>
+        <div class="site-footer__legal">
+            <a href="#">Impressum</a>
+            <a href="#">Datenschutz</a>
+        </div>
+    </div>
+</div>`;
     }
 })();
